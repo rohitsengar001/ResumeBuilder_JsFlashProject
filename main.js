c559493d9ff75c12211620a03cabc91458aef392
rohitsengar001 <rohitsengar001@gmail.com>
2020-11-25 13:02:22 +0530

    appy Indentation on main.js

--- main.js
+++ main.js
@@ -282,77 +282,78 @@ function deleteProjectAndExtraCarr(iconButtonId) {
 //!Use multi-tag input in language field 
 [].forEach.call(document.getElementsByClassName('tags-input'), function (el) {
   let hiddenInput = document.createElement('input'),
-      mainInput = document.createElement('input'),
-      tags = [];
-  
+    mainInput = document.createElement('input'),
+    tags = [];
+
   hiddenInput.setAttribute('type', 'hidden');
   hiddenInput.setAttribute('name', el.getAttribute('data-name'));
 
   mainInput.setAttribute('type', 'text');
   mainInput.classList.add('main-input');
   mainInput.addEventListener('input', function () {
-      let enteredTags = mainInput.value.split(',');
-      if (enteredTags.length > 1) {
-          enteredTags.forEach(function (t) {
-              let filteredTag = filterTag(t);
-              if (filteredTag.length > 0)
-                  addTag(filteredTag);
-          });
-          mainInput.value = '';
-      }
+    let enteredTags = mainInput.value.split(',');
+    if (enteredTags.length > 1) {
+      enteredTags.forEach(function (t) {
+        let filteredTag = filterTag(t);
+        if (filteredTag.length > 0)
+          addTag(filteredTag);
+      });
+      mainInput.value = '';
+    }
   });
 
   mainInput.addEventListener('keydown', function (e) {
-      let keyCode = e.which || e.keyCode;
-      if (keyCode === 8 && mainInput.value.length === 0 && tags.length > 0) {
-          removeTag(tags.length - 1);
-      }
+    let keyCode = e.which || e.keyCode;
+    if (keyCode === 8 && mainInput.value.length === 0 && tags.length > 0) {
+      removeTag(tags.length - 1);
+    }
   });
 
   el.appendChild(mainInput);
   el.appendChild(hiddenInput);
 
-/*     addTag('english'); */
+  /*     addTag('english'); */
 
-  function addTag (text) {
-      let tag = {
-          text: text,
-          element: document.createElement('span'),
-      };
+  function addTag(text) {
+    let tag = {
+      text: text,
+      element: document.createElement('span'),
+    };
 
-      tag.element.classList.add('tag');
-      tag.element.textContent = tag.text;
+    tag.element.classList.add('tag');
+    tag.element.textContent = tag.text;
 
-      let closeBtn = document.createElement('span');
-      closeBtn.classList.add('close');
-      closeBtn.addEventListener('click', function () {
-          removeTag(tags.indexOf(tag));
-      });
-      tag.element.appendChild(closeBtn);
+    let closeBtn = document.createElement('span');
+    closeBtn.classList.add('close');
+    closeBtn.addEventListener('click', function () {
+      removeTag(tags.indexOf(tag));
+    });
+    tag.element.appendChild(closeBtn);
 
-      tags.push(tag);
+    tags.push(tag);
 
-      el.insertBefore(tag.element, mainInput);
+    el.insertBefore(tag.element, mainInput);
 
-      refreshTags();
+    refreshTags();
   }
 
-  function removeTag (index) {
-      let tag = tags[index];
-      tags.splice(index, 1);
-      el.removeChild(tag.element);
-      refreshTags();
+  function removeTag(index) {
+    let tag = tags[index];
+    tags.splice(index, 1);
+    el.removeChild(tag.element);
+    refreshTags();
   }
 
-  function refreshTags () {
-      let tagsList = [];
-      tags.forEach(function (t) {
-          tagsList.push(t.text);
-      });
-      hiddenInput.value = tagsList.join(',');
+  function refreshTags() {
+    let tagsList = [];
+    tags.forEach(function (t) {
+      tagsList.push(t.text);
+    });
+    hiddenInput.value = tagsList.join(',');
   }
 
-  function filterTag (tag) {
-      return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
+  function filterTag(tag) {
+    return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
   }
 });
+
